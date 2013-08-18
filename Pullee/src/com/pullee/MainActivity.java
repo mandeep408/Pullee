32e63177f61771f769b587186c34eda62295f047
Sudjeev Singh <sudjeev.singh22@gmail.com>
2013-08-18 16:21:44 -0700

    Sudjeev Commit
    Changing text in print statement in MainActivity.java

--- Pullee/src/com/pullee/MainActivity.java
+++ Pullee/src/com/pullee/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		System.out.println("TEST TEXT MESSAGE");
+		System.out.println("TEST TEXT MESS");
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
