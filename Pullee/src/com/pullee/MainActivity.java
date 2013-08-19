6982fc7c339b5e30742851adc1d5f98d4d6138aa
Chirag Toor <chiragtoor@gmail.com>
2013-08-18 19:28:25 -0700

    Parse Pulling Data
    Pulling Data from Parse by running a query, getting back a list of
    ParseObjects and displaying the results

--- Pullee/src/com/pullee/MainActivity.java
+++ Pullee/src/com/pullee/MainActivity.java
@@ -1,11 +1,17 @@
 package com.pullee;
 
+import java.util.List;
+
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseException;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +28,15 @@ public class MainActivity extends Activity {
 		ParseAnalytics.trackAppOpened(getIntent());
 		
 		setContentView(R.layout.activity_main);
-		
+		/*
 		ParseObject testObject = new ParseObject("Person");
 		testObject.put("name", "Karan");
 		testObject.put("story", "From India");
 		testObject.put("weight", "average");
 		testObject.saveInBackground();
+		*/
+		
+		
 		
 		scanButton = (Button) this.findViewById(R.id.ScanButton);
 		
@@ -35,12 +44,38 @@ public class MainActivity extends Activity {
 	  		public void onClick(View arg0) {
 	  			//Intent intent = new Intent(MainActivity.this, SelectTypeActivity.class);
 	  			//startActivity(intent);
+	  			
+	  			ParseQuery<ParseObject> query = ParseQuery.getQuery("Person");
+	  			query.whereEqualTo("name", "Sudjeev");
+	  			query.findInBackground(new FindCallback<ParseObject>() {
+	  			    public void done(List<ParseObject> scoreList, ParseException e) {
+	  			        if (e == null) {
+	  			            Log.d("score", "Retrieved " + scoreList.size() + " scores");
+	  			            
+	  			            String text = "";
+	  			            int count = 0;
+	  			            
+	  			            for(ParseObject sTest: scoreList){
+	  			            	
+	  			            	text += sTest.getString("story") + " ";
+	  			            	count++;
+	  			            	
+	  			            }
+	  			            
+	  			            scanButton.setText(count + " " + text);
+	  			        } else {
+	  			            Log.d("score", "Error: " + e.getMessage());
+	  			        }
+	  			    }
+	  			});
+	  			
+	  			/*
 	  			ParseObject testObject = new ParseObject("Person");
 	  			testObject.put("name", "Sudjeev");
 	  			testObject.put("story", "From Mexico");
 	  			testObject.put("height", "tall");
 	  			testObject.saveInBackground();
-	  			
+	  			*/
 	  			
 	  		}
 		});
