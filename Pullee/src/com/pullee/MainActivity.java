a1d4d3c4934994917595faab6b778b19b2aa4760
Chirag Toor <chiragtoor@gmail.com>
2013-08-18 18:43:50 -0700

    Scan Button Integration
    Integrated the scan button into the Activity, put code into the onClick
    method to make it run when button is clicked.

--- Pullee/src/com/pullee/MainActivity.java
+++ Pullee/src/com/pullee/MainActivity.java
@@ -1,15 +1,32 @@
 package com.pullee;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button scanButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		scanButton = (Button) this.findViewById(R.id.ScanButton);
+		
+		scanButton.setOnClickListener(new View.OnClickListener(){
+	  		public void onClick(View arg0) {
+	  			//Intent intent = new Intent(MainActivity.this, SelectTypeActivity.class);
+	  			//startActivity(intent);
+	  			
+	  			
+	  			
+	  		}
+		});
+		
 	}
 
 	@Override
