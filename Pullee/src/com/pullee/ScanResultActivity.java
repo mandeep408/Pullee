5090945b07ce7da6098133bd16101c851013d488
Chirag Toor <chiragtoor@gmail.com>
2013-10-03 13:46:14 -0700

    Image Loading AsyncTask
    Finsihed the AsyncTask in ScanResultActivity so now the pictures load
    without freezing the UI, no black screen or frozen scroll. Users can
    read the story and see all the info while the picture loads with a
    spinner.

--- Pullee/src/com/pullee/ScanResultActivity.java
+++ Pullee/src/com/pullee/ScanResultActivity.java
@@ -1,24 +1,25 @@
 package com.pullee;
 
-import java.util.List; 
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-
-import android.app.Activity;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class ScanResultActivity extends Activity {
 
@@ -37,6 +38,9 @@ public class ScanResultActivity extends Activity {
 	
 	/** Image loading spinner */
 	private ProgressBar spinner;
+	
+	/** Bitmap that holds the downloaded image */
+	private Bitmap loadedBitmap;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -100,47 +104,63 @@ public class ScanResultActivity extends Activity {
 			}
 		});
 		
-		//Launch the AsyncTask for loading the person's image
+		//Launch the AsyncTask for loading the person's image 
+		//  efficiently without freezing the UI
 		new LoadImage().execute();
 		
 	}
 	
 	class LoadImage extends AsyncTask<String, String, String> {
  
-        
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
 
+            //Before loading the image want to make the profilePic invisible 
+            //  and bring up the spinner to show it is loading
     		spinner.setVisibility(View.VISIBLE);
     		profilePic.setVisibility(View.INVISIBLE);
     		
         }
  
-        
         protected String doInBackground(String... args) {
         	
         	//Code to pull down image from Parse
     		ParseQuery<ParseObject> query = ParseQuery.getQuery("Person");
 			query.whereEqualTo("name", Global.name);
-			query.findInBackground(new FindCallback<ParseObject>() {
-			    public void done(List<ParseObject> scoreList, ParseException e) {
-			        if (e == null) {
-			        	if(scoreList.size() > 0){
-			        		ParseObject person = scoreList.get(0);
-			                ParseFile bum = (ParseFile) person.get("Image");
-			                try {
-								byte[] file = bum.getData();
-							    profilePic.setImageBitmap(BitmapFactory.decodeByteArray(file,0,file.length));
-							} catch (ParseException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
+			
+			List<ParseObject> scoreList;
+			
+			try {
+				//use query.find() because we want to run the query on the same Thread, 
+				//  doing in the background would make the code go to onPostExecute() 
+				//  before the image is loaded without waiting for the Bitmap
+				scoreList = query.find();
+				
+				if(scoreList.size() > 0){
+	        		ParseObject person = scoreList.get(0);
+	        		//Get the ParseFile which is the Image of the person
+	                ParseFile bum = (ParseFile) person.get("Image");
+	                //Not everyone has a picture so do a null check
+	                if(bum != null){
+	                	//try catch for the ParseException 
+		                try {
+		                	//get the data in byte form and decode into a Bitmap
+							byte[] file = bum.getData();
+						    loadedBitmap = BitmapFactory.decodeByteArray(file,0,file.length);
+						} catch (ParseException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+	                
+	                }
 
-			        	}
-			        }
-			    }
-			});
+	        	}
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
             
     		return null;
     		
@@ -149,9 +169,21 @@ public class ScanResultActivity extends Activity {
        
         protected void onPostExecute(String file_url) {
 
-        	nameText.setText("done");
-    		spinner.setVisibility(View.INVISIBLE);
-    		profilePic.setVisibility(View.VISIBLE);
+        	//Set the profilePic to be the laodedBitmap with a safety null check
+        	if(loadedBitmap != null){
+        		profilePic.setImageBitmap(loadedBitmap);
+        		
+        		//Get rid of the spinner and show the prerson's picture
+        		spinner.setVisibility(View.INVISIBLE);
+        		profilePic.setVisibility(View.VISIBLE);
+        	} else{
+        		//else if the bitmap is null make both the ImageView and Spinner 
+        		//  invisible
+        		spinner.setVisibility(View.INVISIBLE);
+        		profilePic.setVisibility(View.INVISIBLE);
+        	}
+        	
+        	
     		
         }
  
